cd050b9b0aea2e0d64d33c3d0e60b2dda949ff7f
Kevin <lopezkevin175@gmail.com>
2018-04-04 20:04:24 -0400

    got file structure in order

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../stylesheets/Header.css';
+
+class Header extends Component {
+  render() {
+    return(
+      <header>
+        <h1>this is the header</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
